2befb01905ed62b0498ddeac39e64486cb9958ce
egor1f <gerfedorov0@yandex.ru>
2024-02-13 23:12:56 +0300

    Initial commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,13 @@
+package hiber.service;
+
+import hiber.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+
+    User getCarInfo(String model, int series) throws SQLException;
+}
